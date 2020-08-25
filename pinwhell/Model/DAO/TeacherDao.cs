de0ed9bc6a86af32a4c99c0fba22d5a967e21975
Huy Pham <phamhuy200799@gmail.com>
2020-08-25 16:38:07 +0700

    .

---
+++ pinwhell/Model/DAO/TeacherDao.cs
@@ -0,0 +1,27 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagedList;
+
+namespace Model.DAO
+{
+    public class TeacherDao
+    {
+        PinwhellDbContext db = null;
+        public TeacherDao()
+        {
+            db = new PinwhellDbContext();
+        }
+        public IEnumerable<Teacher> ListAllTeacherPaging(int page, int pageSize)
+        {
+            return db.Teacher.OrderBy(x => x.MaGV).ToPagedList(page, pageSize);
+        }
+        public IEnumerable<TaiLieuHocTap> ListAllTaiLieupaging(int page, int pageSize)
+        {
+            return db.TaiLieuHocTap.OrderByDescending(x => x.MaTL).ToPagedList(page, pageSize);
+        }
+    }
+}
---
+++ pinwhell/pinwhell/Areas/Manage/Controllers/TeacherController.cs
@@ -0,0 +1,25 @@
+﻿using Model.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pinwhell.Areas.Manage.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public TeacherDao db = new TeacherDao();
+        // GET: Manage/Teacher
+        public ActionResult Index(int page = 1, int pageSize=10)
+        {
+            var model = db.ListAllTeacherPaging(page, pageSize);
+            return View(model);
+        }
+        public ActionResult TaiLieuHocTap(int page = 1, int pageSize = 10)
+        {
+            var model = db.ListAllTaiLieupaging(page, pageSize);
+            return View(model);
+        }
+    }
+}
