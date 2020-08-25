7e3da1a8c3f9d1c6526c2ace5188d7e086b82a1e
Duy Nguyen <vanduy290499@gmail.com>
2020-08-25 10:58:49 +0700

    update thu vien

---
+++ pinwhell/Model/DAO/StudentDao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+using PagedList;
+
+namespace Model.DAO
+{
+    class StudentDao
+    {
+        PinwhellDbContext db = null;
+        public StudentDao()
+        {
+            db = new PinwhellDbContext();
+        }
+        public IEnumerable<Student> DanhSachStudent(int page, int pagesize, String Search="")
+        {
+            var list = db.Student.Where(x => x.HoTenHS.Contains(Search)).OrderBy(x => x.Lop).ThenBy(x => x.MaHS).ToPagedList(page, pagesize);
+            return list;
+        }
+
+
+    }
+}
