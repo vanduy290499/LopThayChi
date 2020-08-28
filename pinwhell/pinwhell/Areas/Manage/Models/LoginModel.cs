b2c78348f33d4c5d6c76148f79ff3837a9b6be74
Huy Pham <phamhuy200799@gmail.com>
2020-08-28 15:06:27 +0700

    login

---
+++ pinwhell/pinwhell/Areas/Manage/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace pinwhell.Areas.Manage.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="Vui lòng nhập Tên đăng nhập")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập Mật Khẩu")]
+        public string PassWord { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
